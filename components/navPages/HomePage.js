112eb32fdf0f03c9fa522ee13b111452870f9ef8
mohamed-elsebaey <elsebaey95@outlook.com>
2023-11-14 16:13:24 +0200

    add homePage Button

--- components/navPages/HomePage.js
+++ components/navPages/HomePage.js
@@ -1,9 +1,13 @@
+import Link from "next/link";
 import classes from "./HomePage.module.css";
 function HomePage() {
   return (
     <section className={classes.homePageSection}>
       <div className={classes.homePageContainer}>
         <h1>Welcome to the New Gammal Tech!</h1>
+        <div className={classes.homePageButton}>
+          <Link href="https://new.gammal.tech/home">Visit the homepage</Link>
+        </div>
       </div>
     </section>
   );
