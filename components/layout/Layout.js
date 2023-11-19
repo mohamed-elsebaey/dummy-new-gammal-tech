1c76aa29132a31f1e35d40fdc3dac1849113e689
mohamed-elsebaey <elsebaey95@outlook.com>
2023-11-19 02:16:04 +0200

    update price

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,13 +1,14 @@
-import MainNavigation from '../header/MainNavigation';
-import classes from './Layout.module.css';
-import Footer from '../footer/Footer';
+import MainNavigation from "../header/MainNavigation";
+import classes from "./Layout.module.css";
+import Footer from "../footer/Footer";
 
 function Layout(props) {
   return (
     <div>
+      
       <MainNavigation />
       <main className={classes.body}>{props.children}</main>
-      <Footer/>
+      <Footer />
     </div>
   );
 }
---
+++ components/navPages/signIn/SignIn.js
@@ -0,0 +1,5 @@
+import classes from './SignIn.module.css'
+function SignIn() {
+  return <h1 className={classes.overLay}>sign in</h1>;
+}
+export default SignIn;
